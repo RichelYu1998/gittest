05e2a97d2941b9b1001d42538e1687ae1117eed3
980187223@qq.com <Syp950628>
2020-09-02 11:38:29 +0800

    初始化配置文件

---
+++ test/src/test/test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
